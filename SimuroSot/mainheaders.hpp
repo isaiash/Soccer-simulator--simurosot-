7317e6e6b0333540cdff78c040459a33b151e639
ihuerta <ihuerta@udec.cl>
2018-04-16 19:57:10 -0300

    latest

---
+++ SimuroSot/mainheaders.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef MAINHEADERS
+#define MAINHEADERS
+
+#include "comdef.h"
+#include "../Simurosot/Skills/skillSet.h"
+#include "../Simurosot/Utils/kalman.h"
+#include "../Simurosot/common/include/config.h"
+#include "../Simurosot/Tactics/tactic.h"
+#include "../Simurosot/Tactics/tinclude.h"
+#endif
